c4d7e81a164d8f264d79f0bb0ba060f62ec27fec
dvec <dvecend@gmail.com>
2018-02-27 23:35:32 +0400

    Move requirements.txt to setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,13 @@ from os.path import join, dirname
 
 setup(
     name='text_similarity',
-    version='0.2',
+    version='0.2.1',
+    install_requires=[
+        'requests==2.9.1',
+        'gensim==3.3.0',
+        'PyStemmer==1.3.0',
+        'stop-words==2015.2.23.1'
+    ],
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.md')).read(),
 )
